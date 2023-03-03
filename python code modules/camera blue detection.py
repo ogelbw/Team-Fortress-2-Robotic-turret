948501317dcfd64126543bf43a72da18505655a3
k23074jh <jed.hodges@student.manchester.ac.uk>
2023-03-03 15:14:34 +0000

    create PID class

--- python code modules/camera blue detection.py
+++ python code modules/camera blue detection.py
@@ -1,6 +1,8 @@
 import cv2
 import os
 
+Threshold = 90
+
 for file in os.listdir(r"C:\Users\jed39\Documents\Team-Fortress-2-Robotic-turret\python code modules\test images"):
     original = cv2.imread(os.path.join(r"C:\Users\jed39\Documents\Team-Fortress-2-Robotic-turret\python code modules\test images", file), cv2.IMREAD_UNCHANGED)
 
@@ -20,7 +22,7 @@ for file in os.listdir(r"C:\Users\jed39\Documents\Team-Fortress-2-Robotic-turret
 
     #bgra
     low = (0,0,0,0)
-    high = (255,100,100,255)
+    high = (255, Threshold, Threshold, 255)
     threshold = cv2.inRange(original, low, high)
 
     contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -35,13 +37,13 @@ for file in os.listdir(r"C:\Users\jed39\Documents\Team-Fortress-2-Robotic-turret
         #finding center of moment
         x = int(m["m10"]/(m["m00"]+1e-5))
         y = int(m["m01"]/(m["m00"]+1e-5))
-        cv2.line(original, (x,0), (x,threshold.shape[0]), (0,70,160), thickness=3)
-        cv2.line(original, (0,y), (threshold.shape[1],y), (0,70,160), thickness=3)
+        # cv2.line(original, (x,0), (x,threshold.shape[0]), (0,70,160), thickness=3)
+        # cv2.line(original, (0,y), (threshold.shape[1],y), (0,70,160), thickness=3)
 
     contours = None
 
     q.sort(key=lambda e: e[0], reverse=True)
-    c = q[0][1]
+    c = q[0][1] # list of (contor area, contor)
 
 
     m = cv2.moments(c)
@@ -49,9 +51,10 @@ for file in os.listdir(r"C:\Users\jed39\Documents\Team-Fortress-2-Robotic-turret
     x = int(m["m10"]/(m["m00"]+1e-5))
     y = int(m["m01"]/(m["m00"]+1e-5))
 
-    cv2.line(original, (x,0), (x,threshold.shape[0]), (0,255,0), thickness=3)
-    cv2.line(original, (0,y), (threshold.shape[1],y), (0,255,0), thickness=3)
+    # cv2.line(original, (x,0), (x,threshold.shape[0]), (0,255,0), thickness=3)
+    # cv2.line(original, (0,y), (threshold.shape[1],y), (0,255,0), thickness=3)
     print(f"Area {q[0][0]}")
+    cv2.drawContours(original, c, -1, (0,255,0), 5)
 
     cv2.imshow("Original image", original)
     cv2.imshow("Red image", red)
